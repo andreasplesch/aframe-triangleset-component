eed80d09a911ba33f3ec2b70cc0ade0c13b35450
Andreas Plesch <andreasplesch@netscape.net>
2017-06-17 17:29:12 -0400

    primitive

--- index.js
+++ index.js
@@ -361,3 +361,23 @@ function applyTranslate (geometry, translate, currentTranslate) {
   geometry.verticesNeedsUpdate = true;
 }
 
+//primitive
+
+var extendDeep = AFRAME.utils.extendDeep;
+// The mesh mixin provides common material properties for creating mesh-based primitives.
+// This makes the material component a default component and maps all the base material properties.
+var meshMixin = AFRAME.primitives.getMeshMixin();
+AFRAME.registerPrimitive('a-triangleset', extendDeep({}, meshMixin, {
+  // Preset default components. These components and component properties will be attached to the entity out-of-the-box.
+  defaultComponents: {
+    geometry: {primitive: 'triangleset'}
+  },
+  // Defined mappings from HTML attributes to component properties (using dots as delimiters).
+  // If we set `depth="5"` in HTML, then the primitive will automatically set `geometry="depth: 5"`.
+  mappings: {
+    vertices: 'geometry.vertices',
+    triangles: 'geometry.triangles',
+    uvs: 'geometry.uvs',
+    projectdir: 'geometry.projectdir'
+  }
+}));
